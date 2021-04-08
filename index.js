a6866b6fd14f2683fbe06bf44a645d913ec0798b
daulen <daulenz@gmail.com>
2021-04-08 17:53:28 -0500

    commit tree d4f92c6bfe6a96f76a1da1a0143a8da4c455b4da

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+return 2000/100
+}
+
+const square=x=>x*x;
+
+const add=(x,y)=>(x+y);
+// const 
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2); 
+// squares; //=> [1,4,9]
+// nums; //=> [1,2,3] 
